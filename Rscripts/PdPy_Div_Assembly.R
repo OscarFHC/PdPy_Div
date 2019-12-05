1acf53925e62700f19a2b865b6943a4d2f2e65ce
OscarFHC <oscarhsnurene@gmail.com>
2019-12-05 10:58:13 +0800

    upload hypothesis figures

--- Rscripts/PdPy_Div_Assembly.R
+++ Rscripts/PdPy_Div_Assembly.R
@@ -305,9 +305,9 @@ p_Envi_pairs <- HNF_Bac_A %>%
 p_Envi_pairs
 #ggsave(p_Envi_pairs, file = "D:/Research/PdPy_Div_Results/p_Envi_pairs.jpeg", dpi = 600, width = 34, height = 28, units = "cm")
 
-Envi <- HNF_Bac_A[, c("Temp", "Sal", "PAR", "NO2", "NO3", "PO3")] #, "DIN"
+Envi <- HNF_Bac_A[, c("Temp", "Sal", "PAR", "PO3", "DIN")] #, "NO2", "NO3"
 
-fa <- fa.parallel(Envi, fm = "minres", fa = 'fa')
+fa <- fa.parallel(Envi, fm = "mle", fa = 'fa')
 
 fa1 <- fa(Envi, nfactors = 1, rotate = "varimax", fm = "mle", n.iter = 1000)
 print(fa1)
@@ -319,7 +319,7 @@ fa4 <- fa(Envi, nfactors = 4, rotate = "varimax", fm = "mle", n.iter = 1000)
 print(fa4)
 fa5 <- fa(Envi, nfactors = 5, rotate = "varimax", fm = "mle", n.iter = 1000)
 print(fa5)
-fa.diagram(fa10)
+fa.diagram(fa3)
 ### plotting
 
 
