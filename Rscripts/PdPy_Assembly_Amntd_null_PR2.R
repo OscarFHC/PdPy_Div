0e168ad1668ccfc6db4d301fabfd4e17d6465dbd
OscarFHC <oscarhsnurene@gmail.com>
2020-01-20 12:09:47 +0800

    Update PdPy_Assembly_Amntd_null_PR2.R

--- Rscripts/PdPy_Assembly_Amntd_null_PR2.R
+++ Rscripts/PdPy_Assembly_Amntd_null_PR2.R
@@ -102,7 +102,7 @@ Vars <- read.table(file = "https://raw.githubusercontent.com/OscarFHC/PdPy_Div/m
 ini <- Sys.time()
 numCores <- detectCores()
 numCores
-cl <- makeCluster(numCores)
+cl <- makeCluster(numCores-4)
 
 clusterEvalQ(cl, {
   library(vegan)
@@ -122,21 +122,22 @@ BacPhylo_null_func <- function(x){
   as.matrix(mntd(Bac_comm, cophenetic(tipShuffle(Bac_phylo)), abundance.weighted = TRUE))
 }
 
-nsim.list <- sapply(1:999, list)
+nsim.list <- sapply(1:99, list)
 test <- parLapply(cl, nsim.list, BacPhylo_null_func)
-test[[1000]] <- as.matrix(mntd(Bac_comm, cophenetic(tipShuffle(Bac_phylo)), abundance.weighted = TRUE))
+test[[100]] <- as.matrix(mntd(Bac_comm, cophenetic(Bac_phylo), abundance.weighted = TRUE))
 
 Sys.time() - ini
 
 BacPhylo_null <- data.frame(matrix(unlist(test), ncol = length(test), byrow = FALSE)) %>%
   cbind(row.names(Bac_comm)) %>%
   rename(obs = X1000)
-write.table(BacPhylo_null, file = "D:/Research/PdPy_Div_Results/Bac_Ampd_null.csv", 
+write.table(BacPhylo_null, file = "D:/Research/PdPy_Div_Results/Bac_Amntd_null.csv", 
             sep = ",", col.names = TRUE, row.names = FALSE)
 stopCluster(cl)
 ##### Bacteria phylogenetic turnover ################################################
 
 ##### Nanoflagellate phylogenetic turnover ##########################################
+ini <- Sys.time()
 numCores <- detectCores()
 numCores
 
@@ -159,20 +160,22 @@ NFPhylo_null_func <- function(x){
   as.matrix(mntd(community, cophenetic(tipShuffle(phylo)), abundance.weighted = TRUE))
 }
 
-
 nsim.list <- sapply(1:999, list)
 test <- parLapply(cl, nsim.list, NFPhylo_null_func)
 test[[1000]] <- as.matrix(mntd(NF_comm, cophenetic(NF_phylo), abundance.weighted = TRUE))
 
+Sys.time() - ini
+
 NFPhylo_null <- data.frame(matrix(unlist(test), ncol = length(test), byrow = FALSE)) %>%
-  cbind(expand.grid(row.names(NF_comm), row.names(NF_comm))) %>%
+  cbind(row.names(NF_comm)) %>%
   rename(obs = X1000)
-write.table(NFPhylo_null, file = "D:/Research/PdPy_Div_Results/NF_MPD_null_PR2.csv",
+write.table(NFPhylo_null, file = "D:/Research/PdPy_Div_Results/NF_Amntd_null_PR2.csv",
             sep = ",", col.names = TRUE, row.names = FALSE)
 stopCluster(cl)
 ##### Nanoflagellate phylogenetic turnover ##########################################
 
 ##### Hetero-trophic Nanoflagellate phylogenetic turnover ###########################
+ini <- Sys.time()
 numCores <- detectCores()
 numCores
 
@@ -194,134 +197,20 @@ HNFPhylo_null_func <- function(x){
   # performing comdistnt to calculate MNTD
   as.matrix(mntd(community, cophenetic(tipShuffle(phylo)), abundance.weighted = TRUE))
 }
-ini <- Sys.time()
-nsim.list <- sapply(1:99, list)
+nsim.list <- sapply(1:999, list)
 test <- parLapply(cl, nsim.list, HNFPhylo_null_func)
-Sys.time() - ini
 test[[1000]] <- as.matrix(mntd(HNF_comm, cophenetic(HNF_phylo), abundance.weighted = TRUE))
+
+Sys.time() - ini
 
 HNFPhylo_null <- data.frame(matrix(unlist(test), ncol = length(test), byrow = FALSE)) %>%
   cbind(row.names(HNF_comm)) %>%
   rename(obs = X1000)
-write.table(HNFPhylo_null, file = "D:/Research/PdPy_Div_Results/HNF_Ampd_null_PR2.csv", 
+write.table(HNFPhylo_null, file = "D:/Research/PdPy_Div_Results/HNF_Amntd_null_PR2.csv", 
            sep = ",", col.names = TRUE, row.names = FALSE)
 stopCluster(cl)
 ##### Hetero-trophic Nanoflagellate phylogenetic turnover ###########################
 
 ###############################################################################################
 ##### Phylogenetic turnover / deterministic vs stochastic processes ###########################
-###############################################################################################
-
-###############################################################################################
-##### OTU turnover / Dispersal limitation vs homogeneous dispersal ############################
-###############################################################################################
-
-##### Bacteria OTU turnover #########################################################
-numCores <- detectCores()
-numCores
-
-cl <- makeCluster(numCores)
-
-clusterEvalQ(cl, {
-  library(vegan)
-  library(tidyverse)
-  library(picante)
-  Bac_comm <- as.data.frame(t(read.table(file = "https://raw.githubusercontent.com/OscarFHC/PdPy_Div/master/data/16s_seqXst.csv", sep = ",", 
-                                         header = TRUE, row.names = 1, stringsAsFactors = FALSE, fill = TRUE)))
-})
-
-BacOTU_null_func <- function(x){
-  # set up parameters and data
-  community = Bac_comm
-  n.mod = "independentswap"
-  method = "chao"
-  # performing comdistnt to calculate MNTD
-  as.matrix(vegdist(randomizeMatrix(community, null.model = n.mod), method = method))
-}
-
-nsim.list <- sapply(1:999, list)
-test <- parLapply(cl, nsim.list, BacOTU_null_func)
-test[[1000]] <- as.matrix(vegdist(Bac_comm, null.model = "independentswap", method = "chao"))
-
-BacOTU_null <- data.frame(matrix(unlist(test), ncol = length(test), byrow = FALSE)) %>%
-  cbind(expand.grid(row.names(Bac_comm), row.names(Bac_comm))) %>%
-  rename(obs = X1000)
-# write.table(BacOTU_null, file = "D:/Research/PdPy_Div_Results/Bac_chao_null.csv",
-#             sep = ",", col.names = TRUE, row.names = FALSE)
-stopCluster(cl)
-##### Bacteria OTU turnover #########################################################
-
-##### Nanoflagellate OTU turnover ###################################################
-numCores <- detectCores()
-numCores
-
-cl <- makeCluster(numCores)
-
-clusterEvalQ(cl, {
-  library(vegan)
-  library(tidyverse)
-  library(picante)
-  NF_comm <- as.data.frame(t(read.table(file = "https://raw.githubusercontent.com/OscarFHC/PdPy_Div/master/data/prot_seqXst_PR2.csv", sep = ",", 
-                                        header = TRUE, row.names = 1, stringsAsFactors = FALSE, fill = TRUE)))
-})
-
-
-NFOTU_null_func <- function(x){
-  # set up parameters and data
-  community = NF_comm
-  n.mod = "independentswap"
-  method = "chao"
-  # performing comdistnt to calculate MNTD
-  as.matrix(vegdist(randomizeMatrix(community, null.model = n.mod), method = method))
-}
-
-nsim.list <- sapply(1:999, list)
-test <- parLapply(cl, nsim.list, NFOTU_null_func)
-test[[1000]] <- as.matrix(vegdist(NF_comm, null.model = "independentswap", method = "chao"))
-
-NFOTU_null <- data.frame(matrix(unlist(test), ncol = length(test), byrow = FALSE)) %>%
-  cbind(expand.grid(row.names(NF_comm), row.names(NF_comm))) %>%
-  rename(obs = X1000)
-write.table(NFOTU_null, file = "D:/Research/PdPy_Div_Results/NF_chao_null_PR2.csv",
-            sep = ",", col.names = TRUE, row.names = FALSE)
-stopCluster(cl)
-##### Nanoflagellate OTU turnover ###################################################
-
-##### Hetero-trophic Nanoflagellate OTU turnover ####################################
-numCores <- detectCores()
-numCores
-
-cl <- makeCluster(numCores)
-
-clusterEvalQ(cl, {
-  library(vegan)
-  library(tidyverse)
-  library(picante)
-  HNF_comm <- as.data.frame(t(read.table(file = "https://raw.githubusercontent.com/OscarFHC/PdPy_Div/master/data/HNF_seqXst_PR2.csv", sep = ",", 
-                                        header = TRUE, row.names = 1, stringsAsFactors = FALSE, fill = TRUE)))
-})
-
-HNFOTU_null_func <- function(x){
-  # set up parameters and data
-  community = HNF_comm
-  n.mod = "independentswap"
-  method = "chao"
-  # performing comdistnt to calculate MNTD
-  as.matrix(vegdist(randomizeMatrix(community, null.model = n.mod), method = method))
-}
-
-nsim.list <- sapply(1:999, list)
-test <- parLapply(cl, nsim.list, HNFOTU_null_func)
-test[[1000]] <- as.matrix(vegdist(HNF_comm, null.model = "independentswap", method = "chao"))
-
-HNFOTU_null <- data.frame(matrix(unlist(test), ncol = length(test), byrow = FALSE)) %>%
-  cbind(expand.grid(row.names(HNF_comm), row.names(HNF_comm))) %>%
-  rename(obs = X1000)
-write.table(HNFOTU_null, file = "D:/Research/PdPy_Div_Results/HNF_chao_null_PR2.csv",
-            sep = ",", col.names = TRUE, row.names = FALSE)
-stopCluster(cl)
-##### Hetero-trophic Nanoflagellate OTU turnover ####################################
-
-###############################################################################################
-##### OTU turnover / Dispersal limitation vs homogeneous dispersal ############################
 ###############################################################################################
